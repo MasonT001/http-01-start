bef303056a8e7536a97709df8741d039456fd7bc
MasonT001 <mltrepagnier@gmail.com>
2023-01-30 09:48:59 -0800

    multiple interceptors

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -5,12 +5,8 @@ export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log("Request is on its way!")
         console.log(req.url)
+        console.log(req.headers)
         const modifiedReq = req.clone({headers: req.headers.append('Auth', 'xyz')})
-        return next.handle(modifiedReq).pipe(tap(event => {
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ')
-                console.log(event.body)
-            }
-        }))
+        return next.handle(modifiedReq)
     }
 }
